c9c7d1e1ebaf99000d4b7b60f7f4d880b11b649e
Lucas Rodrigues <lucas.rodrigues6673@gmail.com>
2018-04-28 20:18:04 -0300

    Endpoint test

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 import express from 'express';
-
+import models from './database/models';
 
 const PORT = process.env.PORT || 80;
 const server = express();
+
 server.use(express.static('public'));
-server.listen(PORT);
-console.log(`Listening server in port: ${PORT}`);
+server.get('/deaths', (req, res) => {
+    models.Death.findAll()
+        .then(deaths => res.send(deaths))
+        .catch(err => {
+            res.status(400);
+            res.send(err);
+            console.log(err)
+        });
+})
+
+models.sequelize.sync().then(() => {
+    server.listen(PORT);
+    console.log(`Listening server in port: ${PORT}`);
+});
